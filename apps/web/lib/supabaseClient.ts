9c2344a4b516988864c319cf4663797989e0e8e8
Shivaraj-Kolekar <shivkolekar01@gmail.com>
2025-08-24 17:30:47 +0530

    removed uplaod proxy api

--- apps/web/lib/supabaseClient.ts
+++ apps/web/lib/supabaseClient.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 // These environment variables should be set in your .env.local or deployment environment
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.SUPABASE_URL!;
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY!;
 
 // Create a single Supabase client for the app
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
